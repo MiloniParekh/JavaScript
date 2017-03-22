0869526bd25c105a7eb181330e552975cb507bf1
MiloniParekh <mdparekh97@gmail.com>
2017-03-22 12:29:29 +0530

    Update script.js

--- ClassTest2_A035/script.js
+++ ClassTest2_A035/script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded",
               var temp =
    document.getElementById("name").value;
 
-   document.getElementById("true").checked = true;
+   
     document.getElementById("false").checked = false;
                  
               if ( temp === res.student1) {
@@ -21,12 +21,44 @@ document.addEventListener("DOMContentLoaded",
                 var temp1= "<h3>Question 1: " + res.question1 "</h3>";
                  document.querySelector("#content")
                 .innerHTML = temp1;
+                 function(true){
+                      document.getElementById("true").checked = true;
+                      document.getElementById("false").checked = false;
+                      var display =  "<h4> The result is: Pass</h4>"; 
+
+                      document.getElementById("demo2").innerHTML = display; 
+  
+                       }
+                function(false){
+                      document.getElementById("true").checked = false;
+                      document.getElementById("false").checked = true;
+                      var display =  "<h4> The result is: Fail</h4>"; 
+
+                      document.getElementById("content2").innerHTML = display; 
+  
+                      }
               }
               else if (temp === res.student2) {
                
                 var temp2= "<h3>Question 2: " + res.question2 "</h3>";
                  document.querySelector("#content")
                 .innerHTML = temp2;
+                 function(true){
+                      document.getElementById("true").checked = true;
+                      document.getElementById("false").checked = false;
+                      var display =  "<h4> The result is: Fail</h4>"; 
+
+                      document.getElementById("content2").innerHTML = display; 
+  
+                       }
+                function(false){
+                      document.getElementById("true").checked = false;
+                      document.getElementById("false").checked = true;
+                      var display =  "<h4> The result is: Pass</h4>"; 
+
+                      document.getElementById("demo2").innerHTML = display; 
+  
+                      }
               }
           else {
             var temp3 = "no such student";
@@ -35,6 +67,6 @@ document.addEventListener("DOMContentLoaded",
         
             });
       });
-  
-  }
+ 
+   
 );
